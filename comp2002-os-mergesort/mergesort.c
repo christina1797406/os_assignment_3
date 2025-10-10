c2e95d316fca9b65f5d5678a068d18a291cec8da
christina1797406 <a1797406@adelaide.edu.au>
2025-10-10 17:18:09 +1030

    Add back error.h header

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -7,20 +7,20 @@
 #include <stdlib.h> /* for malloc */
 #include "mergesort.h"
 
-/* this function will be called by mergesort() and also by parallel_mergesort(). */
+/* this function will be called by mergesort() and also by parallel_mergesort(). Christina */
 void merge(int leftstart, int leftend, int rightstart, int rightend){
 }
 
-/* this function will be called by parallel_mergesort() as its base case. */
+/* this function will be called by parallel_mergesort() as its base case. Minh */
 void my_mergesort(int left, int right){
 }
 
-/* this function will be called by the testing program. */
+/* this function will be called by the testing program. Samira */
 void * parallel_mergesort(void *arg){
 		return NULL;
 }
 
-/* we build the argument for the parallel_mergesort function. */
+/* we build the argument for the parallel_mergesort function. TBD */
 struct argument * buildArgs(int left, int right, int level){
 		return NULL;
 }
