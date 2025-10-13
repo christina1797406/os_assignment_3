a7d61da8d14c2f9f24dc5c437fe4adf0e046efe7
Nguyễn Hồng Minh <nguyenhongminh@Nguyens-MacBook-Air-3.local>
2025-10-13 11:09:34 +1030

    Minh - my_mergesort

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -13,6 +13,12 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 
 /* this function will be called by parallel_mergesort() as its base case. Minh */
 void my_mergesort(int left, int right){
+	if (left >= right) return;
+
+    int mid = left + (right - left) / 2;
+    my_mergesort(left, mid);
+    my_mergesort(mid + 1, right);
+    merge(left, mid, mid + 1, right);
 }
 
 /* this function will be called by the testing program. Samira */
