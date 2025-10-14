526d13f91e6936d180c85b4b52d548c78eeca6a9
Samira <a1852922@adelaide.edu.au>
2025-10-14 14:36:10 +1030

    fixed merge

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -8,16 +8,14 @@
 #include <stdlib.h> /* for malloc */
 #include <string.h> /* for memcpy */
 
-<<<<<<< HEAD
-    /* this function will be called by mergesort() and also by
-     * parallel_mergesort(). Christina */
-    void
-    merge(int leftstart, int leftend, int rightstart, int rightend) {}
+/* this function will be called by mergesort() and also by
+ * parallel_mergesort(). Christina */
+void merge(int leftstart, int leftend, int rightstart, int rightend) {}
 
 /* this function will be called by parallel_mergesort() as its base case. Minh
  */
 void my_mergesort(int left, int right) {}
-=======
+
 /* this function will be called by mergesort() and also by parallel_mergesort().
  * Christina */
 void merge(int leftstart, int leftend, int rightstart, int rightend) {
@@ -58,7 +56,6 @@ void my_mergesort(int left, int right) {
   my_mergesort(mid + 1, right);
   merge(left, mid, mid + 1, right);
 }
->>>>>>> 9e6ec3d1745b553423c7a32df8f52c405eab9e46
 
 /* this function will be called by the testing program. Samira */
 void *parallel_mergesort(void *arg) {
@@ -80,8 +77,8 @@ void *parallel_mergesort(void *arg) {
   struct argument *leftArg = buildArgs(left, mid, level + 1);
   struct argument *rightArg = buildArgs(mid + 1, right, level + 1);
 
-  pthread_t t1, ;
-  t2 int rc;
+  pthread_t t1, t2;
+  int rc;
 
   rc = pthread_create(&t1, NULL, parallel_mergesort, leftArg);
   if (rc) {
